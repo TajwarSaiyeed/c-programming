718766c55e383e0d3548d28e56c9ea39981e0ece
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-20 20:20:07 +0600

    cls-1

---
+++ cls-1/dataType.c
@@ -0,0 +1,9 @@
+
+#include<stdio.h>
+
+int main() {
+    int integer=5;
+    float pi = 3.1416;
+    char c = "A";
+    return 0;
+}
---
+++ cls-1/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int number = 25;
+    float pi = 3.1416;
+    int _age = 22;
+    _age = 24;
+    char _address = "A";
+    int final_price = 100;
+    return 0;
+}
