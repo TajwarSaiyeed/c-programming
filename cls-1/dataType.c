62e90702e70f99f85073372aeb58f6cf3e527b0d
TajwarSaiyeed <rjabid36@gmail.com>
2023-01-20 20:37:05 +0600

    update | new line

--- cls-1/dataType.c
+++ cls-1/dataType.c
@@ -1,23 +1,22 @@
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
-    int integer=5,a;
+int main()
+{
+    int integer = 5, a;
     float pi = 3.1416;
-    char c = "A";
-    a = 25.666;
-
-
+    char star = "A";
     // This it single line comments
     /*
-
         multi-line comment
-
     */
-
     // show output
     // new line \n
     printf("hello world\n");
     printf("hello abid");
+    printf("The value of integer is: %d\n", integer);
+    printf("The value pi is: %f\n", pi);
+    printf("The value Star is: %c\n", star);
+
     return 0;
 }
