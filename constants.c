3b889e73a2eac20a962f8d940cdb89824b38369c
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-04 21:51:19 +0600

    input | print function | scanf | comments

---
+++ constants.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+{
+    int abid = 7;
+    char abid_Char = 'a'; // comment
+    float abid_float = 4.44;
+    int Abid = 8;
+
+    printf("abid: %d\nabid_Char: %c\nabid_float: %.2f\nAbid: %d", abid, abid_Char, abid_float, Abid);
+
+
+    return 0;
+}
+
+
---
+++ input.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main() {
+    printf("Please type your mobile number: ");
+    int number;
+    scanf("%d", &number);
+    printf("Your Number is : %d", number);
+
+    getch();
+
+
+}
