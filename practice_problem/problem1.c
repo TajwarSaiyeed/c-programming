eded238072ab8588967e18f2e85bd3b039b587dc
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-04 21:59:31 +0600

    problem 1

---
+++ practice_problem/problem1.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main() {
+
+    float length, breadth;
+    printf("length of Rectangle: ");
+    scanf("%f", &length);
+    printf("breadth of Rectangle: ");
+    scanf("%f", &breadth);
+
+    printf("Area of Rectangle: %.3f", length * breadth);
+
+    getch();
+}
