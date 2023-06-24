693569bce7df61dda114ceccc4463941b54f1d61
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-24 16:13:03 +0600

    chatper 4 practice

---
+++ chapter4/pratice/03_practice.c
@@ -0,0 +1,12 @@
+/*
+    Question:
+        A do while loop is executed:
+            1. at least one
+            2. at least twice
+            3. at most once.
+
+
+    Answer:
+        => 1. at least once.
+
+*/
