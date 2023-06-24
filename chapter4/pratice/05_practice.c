af139b5cd01c4cfeedd27db876e93cf877333db1
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-24 16:26:49 +0600

    chapter 4 | problem 5

---
+++ chapter4/pratice/05_practice.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/*
+    Write a program to sum first ten natural
+    numbers using (while loop);
+*/
+
+
+int main() {
+
+    int i = 1, sum;
+    while(i<=10) {
+        sum +=i;
+        i++;
+    }
+
+    printf("The sum of first ten natural numbers is : %d", sum);
+
+    return 0;
+}
