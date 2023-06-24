f7d2d95529c59993db14c19b8fec28cacb238ba1
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-24 16:32:34 +0600

    chapter 4 | problem 5, 6, 7

--- chapter4/pratice/06_practice.c
+++ chapter4/pratice/06_practice.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 /*
     Write a program to sum first ten natural
-    numbers using (while loop);
+    numbers using (for loop);
 */
 
 
 int main() {
 
-    int i = 1, sum =0;
-    while(i<=10) {
+    int sum = 0;
+
+    for(int i = 1; i <= 10; i++) {
         sum +=i;
-        i++;
     }
 
     printf("The sum of first ten natural numbers is : %d", sum);
--- chapter4/pratice/07_practice.c
+++ chapter4/pratice/07_practice.c
@@ -1,19 +1,21 @@
 #include<stdio.h>
 /*
     Write a program to sum first ten natural
-    numbers using (while loop);
+    numbers using (do while loop);
 */
 
 
 int main() {
 
-    int i = 1, sum =0;
-    while(i<=10) {
+    int sum = 0, i=1;
+
+    do{
         sum +=i;
         i++;
-    }
+    }while(i<=10);
 
     printf("The sum of first ten natural numbers is : %d", sum);
 
     return 0;
 }
+
