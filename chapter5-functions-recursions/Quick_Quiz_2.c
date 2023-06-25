9487a8aa9f407de2e1bbd72e00f56fa634ddadda
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-25 21:40:41 +0600

    Quick Quiz 2

---
+++ chapter5-functions-recursions/Quick_Quiz_2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<math.h>
+
+/*
+    Quick Quiz:
+        Use a library function to calculate the area
+        of a square with side a.
+*/
+
+
+int main() {
+    double a =2 ;
+    printf("The area of the square is: %lf", pow(a,2));
+
+    return 0;
+}
