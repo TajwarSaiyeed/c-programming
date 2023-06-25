5d8d5ae21ce7eecf7d5ead5114ab49d5f6759796
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-25 19:45:44 +0600

    Important Points in functions

---
+++ chapter5-functions-recursions/Important_points.c
@@ -0,0 +1,22 @@
+/*
+    Important points
+
+        1. Execution of a C program starts from main()
+        2. A C program can have more than one function
+        3. Every function gets called directly or indirectly from main()
+        4. There are two types of functions in C.
+
+    Let't talk about them.
+
+    Types of functions
+
+        Library Functions -> commonly required functions grouped together in a library file on disk.
+
+        User defied functions -> These are the functions declared and defined by the users.
+
+    Why use functions?
+
+        1. To avoid rewriting the same logic again and again.
+        2. To keep track of what we are doing in a program.
+        3. To test and check logic independently.
+*/
