cbb2791039c9902424e07ebd6316344e10f77239
TajwarSaiyeed <rjabid36@gmail.com>
2023-06-27 11:37:41 +0600

    practice 2 celsius to Fahrenheit

---
+++ chapter5-functions-recursions/practice/practice2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+/*
+    Write a program using function to convert
+    Celsius temperature into Fahrenheit.
+*/
+
+float c2f(float);
+
+
+int main() {
+    float celsius =  37;
+    float fahrenheit = c2f(celsius);
+    //(0C X 9/5)+32 = 32F
+
+    printf("The value of %.3f degree Celsius in Fahrenheit is %f", celsius, fahrenheit);
+
+
+    return 0;
+}
+
+
+
+float c2f(float c) {
+    float f =(c * 9/5.0)+32;
+    return f;
+}
