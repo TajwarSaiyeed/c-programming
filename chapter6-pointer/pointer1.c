c35571d18015d41c5a06bfe969602e2b7108fa11
TajwarSaiyeed <rjabid36@gmail.com>
2023-07-02 09:17:57 +0600

    pointer details | pointer test app

---
+++ chapter6-pointer/pointer1.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/*
+    How to declare a pointer?
+
+    A pointer is declared using the following syntax
+
+    int *j; declare a variable j of type int-pointer
+    j = &i; store address of i in j
+
+    Just like pointer of type integer, we also have pointers
+    to char, float etc.
+
+    int *ch-ptr; pointer to integer
+    char *ch-ptr; pointer to character
+    float *ch-ptr; pointer to float
+
+    Although its a good practice to use meaningful variable names,
+    we should be very careful while reading & working on programs
+    from fellow programmers.
+*/
+
+
+// creating pointer
+
+int main() {
+    int i = 72;
+    int *int_ptr = 5;
+    int *j = &i;
+    printf("The value of i is %d\n", *j);
+    printf("The address of i is %u\n", j);
+    printf("The value of i is %d\n", i);
+    printf("The address of i is %u\n", &i);
+    printf("The address of i is %u\n", &int_ptr);
+
+    return 0;
+}
+
---
+++ chapter6-pointer/pointer_test_app.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+
+int main() {
+
+    int i = 8;
+    int *j; // int pointer
+    j=&i; // address of i in j
+
+    printf("Add i = %u\n", &i); // address of i
+    printf("Add i = %u\n", j); // address of i
+    printf("Add j = %u\n", &j); // address of j
+    printf("Value i = %d\n", i); // 8
+    printf("Value i = %d\n", *(&i)); // 8
+    printf("Value i = %d\n", *j); // 8
+
+
+    /*
+        This program sums it all.
+        If you understand it, you have got the idea of pointers.
+    */
+
+    return 0;
+}
