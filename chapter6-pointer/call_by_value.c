73c412179161d8a8205b1bb1041c665d46a1e767
TajwarSaiyeed <rjabid36@gmail.com>
2023-07-03 21:20:34 +0600

    update

--- chapter6-pointer/call_by_value.c
+++ chapter6-pointer/call_by_value.c
@@ -4,12 +4,14 @@ int sum(int, int);
 
 int main() {
     int x=10, y = 20;
-    printf("The value of a + b is %d",sum(x,y));
+    printf("The value of x + y is %d\n",sum(x,y));
+    printf("The value of x and y is %d and %d",x, y);
 
 
     return 0;
 }
 
 int sum(int x, int y) {
+    x=26;
     return x+y;
 }
